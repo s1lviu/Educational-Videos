1cc84fd39d0d5339dfb563ae38f593b898f392d1
Stroe Silviu Bogdan <silviu@silviu-s.com>
2016-02-19 20:07:48 +0200

    Beta 1

--- www/js/app.js
+++ www/js/app.js
@@ -21,10 +21,12 @@ app.run(function ($ionicPlatform) {
       StatusBar.styleDefault();
     }
   });
+
+
 });
 
 
-app.controller('MainCtrl', function ($scope, $cordovaFileOpener2) {
+app.controller('MainCtrl', function ($scope, $ionicPopup, $cordovaFileOpener2, $cordovaFile, $ionicPlatform) {
   var currentStart = 0;
   $scope.items = [];
 
@@ -42,27 +44,67 @@ app.controller('MainCtrl', function ($scope, $cordovaFileOpener2) {
      */
 
     $scope.data = {
-      "Triunghi": "triunghi.mp4"
+      "Ortocentrul, înălţimile triunghiului": "triunghi.mp4"
     };
 
   };
 
   $scope.addItems();
 
-  $scope.open = function (text) {
-    //var file = "/android_asset/www/videos/" + "" + text;
-    var file = '/android_asset/www/videos/triunghi.mp4';
 
-    $cordovaFileOpener2.open(
-      file,
-      'video/mp4'
-    ).then(function () {
-      console.log("File opened succesfully!");
-    }, function (err) {
-      console.log("Error!" + "File " + file + " " + err.message);
-    });
+  $ionicPlatform.ready(function () {
 
-  }
+    /*
+     $scope.success = function () {
+     $ionicPopup.alert({
+     title: 'Succes!',
+     content: 'Fisierele au fost procesate!'
+     });
+     };
+     $scope.error = function () {
+     $ionicPopup.alert({
+     title: 'Ne pare rau!',
+     content: 'A aparut o eroare la copierea fisierelor!'
+     });
+     };
+
+
+     var sursa = cordova.file.applicationDirectory + "www";
+     var destinatie = cordova.file.externalDataDirectory;
+     console.log(sursa);
+     console.log(destinatie);
+     $cordovaFile.copyDir(sursa, "videos", destinatie, "videoclipuri")
+     .then(function (success) {
+     $scope.success();
+     console.log(success);
+     }, function (error) {
+     $scope.error();
+     console.log(error);
+     });
+     */
+
+    $scope.open = function (text) {
+      var file = destinatie + "videoclipuri/" + text;
+
+      console.log(file);
+
+      window.open(file, '_system', 'location=no');
+
+      /*
+       $cordovaFileOpener2.open(
+       file,
+       'video/mp4'
+       ).then(function () {
+       console.log("File opened succesfully!");
+       }, function (err) {
+       console.log("Error!" + "File " + file + " " + err.message);
+       });
+
+       */
+
+    }
+
+  });
 
 });
 
